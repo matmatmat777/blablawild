873db397073e7f62f2010ef1e1a12343c0aedfee
Mathieu <matdelmas@gmail.com>
2017-05-05 16:39:51 +0200

    date picker dialog

--- app/src/main/java/com/example/apprenti/blablawild/SearchItineraryActivity.java
+++ app/src/main/java/com/example/apprenti/blablawild/SearchItineraryActivity.java
@@ -1,38 +1,75 @@
 package com.example.apprenti.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.support.v7.widget.ContentFrameLayout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import static com.example.apprenti.blablawild.R.id.editTextDate;
 
 public class SearchItineraryActivity extends AppCompatActivity {
 
     EditText editText;
     EditText editText2;
-    EditText editText3;
+    EditText editTextDate;
     Button button2;
     Context context;
     Toast toast;
     Intent Resultat;
     int toastDuration;
+    Calendar mCalendar;
+    DatePickerDialog.OnDateSetListener date;
     public final static String EXTRA_REQUEST = "resultat";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_itinerary);
 
-        editText =(EditText)findViewById(R.id.editText);
-        editText2 =(EditText)findViewById(R.id.editText2);
-        editText3 = (EditText)findViewById(R.id.editText3);
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        editTextDate = (EditText) findViewById(R.id.editTextDate);
         button2 = (Button) findViewById(R.id.button2);
         context = getApplicationContext();
         toast = Toast.makeText(context, getString(R.string.toast), toastDuration);
         toastDuration = Toast.LENGTH_SHORT;
+        mCalendar = Calendar.getInstance();
+
+        date = new DatePickerDialog.OnDateSetListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                mCalendar.set(Calendar.YEAR, year);
+                mCalendar.set(Calendar.MONTH, month);
+                mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+
+            }
+        };
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(SearchItineraryActivity.this, date,
+                        mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+
+
+
 
         Resultat = new Intent(this, ViewSearchItineraryResultsListActivity.class);
 
@@ -45,7 +82,7 @@ public class SearchItineraryActivity extends AppCompatActivity {
 
                     String mDepart = editText.getText().toString();
                     String mArrivee = editText2.getText().toString();
-                    String mDateDepart = editText3.getText().toString();
+                    String mDateDepart = editTextDate.getText().toString();
 
                     SearchRequestModel resultat = new SearchRequestModel (mDepart, mArrivee, mDateDepart);
 
@@ -63,7 +100,18 @@ public class SearchItineraryActivity extends AppCompatActivity {
                 }
             }
 
-        });}}
+        });
+    }
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editTextDate.setText(sdf.format(mCalendar.getTime()));
+    }
+}
+
+
 
 
 
